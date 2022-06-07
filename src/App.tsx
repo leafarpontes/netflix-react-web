dbba44a386d85b4bf63f6ba074335cdaac7bf710
leafarpontes <rafapontes2008@hotmail.com>
2022-06-07 20:22:39 -0300

    Added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 import Login from './screens/login/login.screen';
+import { GlobalStyles } from './themes/main/global-styles';
 import theme from './themes/main/theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Login />
-    </ThemeProvider>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
+    </>
   );
 }
 
